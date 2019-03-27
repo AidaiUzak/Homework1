3b22e158cd5c2daf166c78dde166b12333a4ce9d
AidaiUzak <aidaibaidzhigitova@gmail.com>
2019-03-27 18:45:22 +0600

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        final String variable1 = "Привет,";
+        String variable2 = "мир !";
+        String variable = variable1 + variable2;
+
+        System.out.println(variable);
+        System.out.println(variable1);
+    }
+}
